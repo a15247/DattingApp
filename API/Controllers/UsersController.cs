43e6eecdc9d6232ef4b06570ebd927abfc8bb7ad
João Gomes <a15247@alunos.ipca.pt>
2023-02-27 14:28:10 +0000

    end

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]     //aceder -> localhost/apt/users(nomedaclass)
-    public class UsersController
+    public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
 
